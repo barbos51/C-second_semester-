ec8948c557d4ff78b9242f70bd1e5e633fba81fb
barbos51 <145984418+barbos51@users.noreply.github.com>
2025-03-05 13:44:48 +0200

    Add files via upload

---
+++ task2/Product.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2
+{
+    class Product
+    {
+        private string _name;
+        private int _quantity;
+        private float _price;
+        public int Quantity => _quantity;
+        //public float Price => _price;
+        //public string Name => _name;
+
+
+
+        public Product(string name, float price, int quantity)
+        {
+            _name = name;
+            _quantity = quantity;
+            _price = price;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return _name; 
+                
+            }
+            set 
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    Console.WriteLine("Введіть назву товару");
+                }
+                else
+                {
+                    _name = value;
+                }
+            }
+        }
+        public float Price
+        {
+            get
+            {
+                return _price;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("Ціна не може бути від'ємною!");
+                }
+                else
+                {
+                    _price = value;
+                }
+            }
+        }
+
+        public void Restock(int amount)
+        {
+            if (amount < 0)
+            {
+                Console.WriteLine("Помилка: Додаюча кількість повинна бути невід'ємною!");
+                return;
+            }
+            _quantity += amount;
+
+        }
+        public void Sell(int amount)
+        {
+            if (Quantity < amount)
+            {
+                Console.WriteLine("Помилка: кількість товару на продаж повинна бути менша за кількість товару на складі");
+                return;
+            }
+            _quantity -= amount;
+        }
+
+     
+
+        public float TotalValue()
+        {
+            return Quantity * _price;
+        }
+
+        public string GetInfo()
+        {
+            return $"Товар: {Name}, Ціна: {Price}, Кількість: {Quantity}, Загальна вартість: {TotalValue()} ";
+        }
+
+    }
+}
---
+++ task2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+namespace task2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = UTF8Encoding.UTF8;
+            Product apple = new Product("Яблуко", 5, 100);
+            Console.WriteLine(apple.GetInfo());
+
+            apple.Sell(20);
+            Console.WriteLine(apple.GetInfo());
+
+
+            apple.Restock(70);
+            Console.WriteLine(apple.GetInfo());
+
+
+            apple.Price = 12; // Зміна ціни товару
+            Console.WriteLine(apple.GetInfo());
+
+            apple.Name = "Зелене яблуко"; // Перейменування товару
+            Console.WriteLine(apple.GetInfo());
+
+            apple.Price=-12;
+            
+        }
+    }
+}
